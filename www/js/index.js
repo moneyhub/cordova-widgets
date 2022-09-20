43734ce4bfc9cb0fc22aa15e53e4dac395c158c0
Omar Zamarripa <omar.zamarripa@moneyhub.com>
2022-09-20 15:12:28 +0300

    Enable receiving payments callback

--- www/js/index.js
+++ www/js/index.js
@@ -17,19 +17,66 @@
  * under the License.
  */
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+const CONFIG = {
+    CLIENT_ID: "client-id",
+    CLIENT_SECRET: "client-secret",
+    PAYEE_ID: "payee-id",
+    REDIRECT_URI: "redirect-uri",
+}
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-    document.addEventListener("pause", onPause, false);
-    document.addEventListener("resume", onResume, false);
-    document.addEventListener("menubutton", onMenuKeyDown, false);
+function parseQuery(queryString) {
+    var query = {};
+    var pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+    for (var i = 0; i < pairs.length; i++) {
+        var pair = pairs[i].split('=');
+        query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+    }
+    return query;
+}
 
-    console.log('Device ready Running cordova-' + cordova.platformId + '@' + cordova.version);
 
-    window.IonicDeeplink.onDeepLink((data) => console.log("deepLink", data))
+function handleDeepLink(data) {
+    console.log("Deep Link", data)
+    const {queryString, host} = data
+
+    if (host === "callback") {
+        const {code, state, id_token: idToken, error} = queryString ? parseQuery(queryString) : {}
+        moneyhubCallback({code, state, idToken, error})
+    }
+}
+
+function openLinkInBrowser(link) {
+    cordova.InAppBrowser.open(link, '_system', 'location=yes');
+}
+
+function initialiseMoneyhubWidget() {
+
+    window.moneyhub.init({
+        elem: document.getElementById("app"),
+        clientId: CONFIG.CLIENT_ID,
+        clientSecret: CONFIG.CLIENT_SECRET,
+        payeeId: CONFIG.PAYEE_ID,
+        payeeRef: "Payee ref",
+        payerRef: "Payer ref",
+        startText: "Start text",
+        amount: 100,
+        type: "api",
+        redirectUri: CONFIG.REDIRECT_URI,
+        openUri: openLinkInBrowser,
+        getJWT: () =>
+      })
+}
+
+function moneyhubCallback({code, state, idToken, error}) {
+
+    window.moneyhub.callback({
+        elem: document.getElementById("app"),
+        code,
+        state,
+        error,
+        idToken,
+        redirectUri: CONFIG.REDIRECT_URI,
+    })
 }
 
 function onPause() {
@@ -42,7 +89,26 @@ function onResume() {
 
 function onMenuKeyDown() {
     // Handle the menubutton event
+    window.moneyhubCallback.goBack()
 }
+
+function onDeviceReady() {
+
+    document.addEventListener("pause", onPause, false);
+    document.addEventListener("resume", onResume, false);
+    document.addEventListener("menubutton", onMenuKeyDown, false);
+
+    // @ts-ignore
+    window.IonicDeeplink.onDeepLink(handleDeepLink)
+
+    initialiseMoneyhubWidget()
+
+    console.log('Device ready Running cordova-' + cordova.platformId + '@' + cordova.version);
+}
+
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener('deviceready', onDeviceReady, false);
 
 // listen for uncaught cordova callback errors
 window.addEventListener("cordovacallbackerror", function (event) {
